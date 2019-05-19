e2785ce6ade17282d0f496b030d86931b1eaf5c9
iakov <kalterxad34@gmail.com>
2019-05-19 20:51:25 +0300

    exercise2 partly done

--- src/app/exercise1/matrix.service.ts
+++ src/app/exercise1/matrix.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { pow, MathType, multiply } from 'mathjs';
+import { MathType, multiply, pow } from 'mathjs';
 
 import { MatrixBase } from './matrix-base';
 
@@ -17,13 +17,12 @@ export const INITIAL_STATE_DISPERSION = [0.8, 0.2, 0, 0, 0];
   providedIn: 'root'
 })
 export class MatrixService {
- private initial = INITIAL_MATRIX;
+  private initial = INITIAL_MATRIX;
 
   constructor() { }
 
-  getVals() {
+  getVals(initial = this.initial) {
     const matrix: MatrixBase<any>[][] = [];
-    const initial: any = this.initial;
 
     for (let i = 0; i < initial.length; i++) {
       const row: any[] = [];
@@ -85,5 +84,57 @@ export class MatrixService {
 
   private getNextDispersion(matrix: MathType, stateDispersion: number[] | MathType): any {
     return multiply(stateDispersion, matrix);
+  }
+
+  private getPi1(p: any) {
+    const top = +p['20'] * +p['40'];
+    const bottom1 = +p['20'] * +p['41'];
+
+    const bot1 = +p['12'] + +p['14'];
+    const bot2 = +p['30'] + +p['32'] + +p['34'];
+
+    const t1 = +p['20'] * +p['40'];
+    const t2 = +p['12'] * +p['40'];
+    const t3 = +p['14'] * +p['20'];
+    const subBot1 = +p['01'] * (t1 + t2 + t3) / bot1;
+
+    const t4 = +p['20'] * +p['40'];
+    const t5 = +p['32'] * +p['40'];
+    const t6 = +p['34'] * +p['20'];
+    const subBot2 = +p['03'] * (t4 + t5 + t6) / bot2;
+
+    const bottom = bottom1 + subBot1 + subBot2;
+    return bottom / top;
+  }
+
+  private getPi2(p: any, pi1: number) {
+    return pi1 * +p['01'] / (+p['12'] + +p['14']);
+  }
+
+  private getPi4(p: any, pi1: number) {
+    return pi1 * +p['03'] / (+p['30'] + +p['32'] + +p['34']);
+  }
+
+  private getPi3(p: any, pi2: number, pi4: number) {
+    return (pi2 * +p['12'] + pi4 * +p['32']) / +p['20'];
+  }
+
+  private getPi5(p: any, pi2: number, pi4: number) {
+    return (pi2 * +p['14'] + pi4 * +p['34']) / +p['40'];
+  }
+
+  getFinalDispersion(p: any): number[] {
+    const pi1 = this.getPi1(p);
+    const pi2 = this.getPi2(p, pi1);
+    const pi4 = this.getPi4(p, pi1);
+    const pi3 = this.getPi3(p, pi2, pi4);
+    const pi5 = this.getPi5(p, pi2, pi4);
+
+    return [pi1, pi2, pi3, pi4, pi5];
+  }
+
+  getSumOfArr(arr: number[]): number {
+    const reducer = (accumulator: number, currentValue: number) => accumulator + currentValue;
+    return arr.reduce(reducer);
   }
 }
--- src/app/exercise2/exercise2.component.ts
+++ src/app/exercise2/exercise2.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { MatrixBase } from '../exercise1/matrix-base';
+import { MatrixControlService } from '../exercise1/matrix-control.service';
+import { MatrixService } from '../exercise1/matrix.service';
+
+const INITIAL_MATRIX = [
+  [0.8, 0.1, 0, 0.1, 0],
+  [0, 0.3, 0.6, 0, 0.1],
+  [0.8, 0, 0.2, 0, 0],
+  [0.7, 0, 0.1, 0.1, 0.1],
+  [0.1, 0, 0, 0, 0.9],
+];
 
 @Component({
   selector: 'app-exercise2',
   templateUrl: './exercise2.component.html',
-  styleUrls: ['./exercise2.component.scss']
+  styleUrls: ['./exercise2.component.scss'],
+  providers: [MatrixControlService, MatrixService]
 })
 export class Exercise2Component implements OnInit {
+  matrixVals: MatrixBase<any>[][] = [];
+  transitionMatrixForm: FormGroup;
 
-  constructor() { }
+  matrix: number[][] = INITIAL_MATRIX;
+  sumOfRows: number[] = [];
+
+  finalDispersion: number[] = [];
+  sumOfDispersion: number;
+
+  constructor(private mcs: MatrixControlService, private ms: MatrixService) {
+    this.sumOfRows = this.ms.getSumOfRows(this.matrix);
+  }
 
   ngOnInit() {
+    this.matrixVals = this.ms.getVals(this.matrix);
+    this.transitionMatrixForm = this.mcs.toFormGroup(this.matrixVals);
+    this.finalDispersion = this.ms.getFinalDispersion(this.transitionMatrixForm.value);
+    this.sumOfDispersion = this.ms.getSumOfArr(this.finalDispersion);
+
+    this.transitionMatrixForm.valueChanges.subscribe(vals => {
+      this.onChangeTransitionMatrix(vals);
+    });
+  }
+
+  onChangeTransitionMatrix(vals) {
+    this.matrix = this.ms.toMatrix(vals);
+    this.sumOfRows = this.ms.getSumOfRows(this.matrix);
+    this.finalDispersion = this.ms.getFinalDispersion(vals);
+    this.sumOfDispersion = this.ms.getSumOfArr(this.finalDispersion);
   }
 
 }
